9a7dc8c45fa8111bf2a0cd6a81f8346398c23a66
habiba <122211697+Habiba-K@users.noreply.github.com>
2024-11-26 14:20:46 +0500

    commit

--- src/app/Components2/Projects2.tsx
+++ src/app/Components2/Projects2.tsx
@@ -5,15 +5,15 @@ import Card from '../Components2/Card2'
 const data = [
     {
         id: 0,
-        img: "/image/th (6).jfif",
+        img: "/image/where-is-ai-used.jpg",
     },
     {
         id: 1,
-        img: "/image/coding-7-1920.webp",
+        img: "/image/15c57871-d733-4fdc-8883-4a2040cd3d95.jfif",
     },
     {
         id: 2,
-        img: "/image/th (2).jfif",
+        img: "/image/AdobeStock_164909094-scaled.jpeg",
     },
 ]
 
